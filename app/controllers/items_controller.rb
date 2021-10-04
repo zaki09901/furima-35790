fe0699ce9ad9675f0c02ba334944eca8993345ab
zaki09901 <85482210+zaki09901@users.noreply.github.com>
2021-10-04 19:52:08 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id ==current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
