befe20139cc204a22414bf9315036195a7c9505f
zaki09901 <85482210+zaki09901@users.noreply.github.com>
2021-10-05 16:24:00 +0900

    １回目修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,10 +40,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
-
+      @item.destroy if @item.user_id ==current_user.id
+      redirect_to root_path
   end
+end
+
 
   private
 
