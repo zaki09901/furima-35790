39aa511182ab3e0581b61d40edec12e2dcc7d8f2
zaki09901 <85482210+zaki09901@users.noreply.github.com>
2021-10-11 16:11:36 +0900

    2回目修正

--- spec/factories/orders_addresses_spec.rb
+++ spec/factories/orders_addresses_spec.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_address do
     token {"tok_abcdefghijk00000000000000000"}
-    prefecture_id                        {'5'}
+    prefecture_id                        {5}
     municipality                         {'横浜市'}
     address                              {'青山1-1-1'}
     phone_number                         {'09012345678'}
