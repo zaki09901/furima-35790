f631537a0d9616c1d0816a566660f6596ac53803
zaki09901 <85482210+zaki09901@users.noreply.github.com>
2021-10-13 16:56:43 +0900

    最終修正

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,8 +2,8 @@ class Ship < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
     { id: 1, name: '1~2日で発送' },
-    { id: 2, name: '2~3で発送' },
-    { id: 3, name: '4~7で発送' }
+    { id: 2, name: '2~3日で発送' },
+    { id: 3, name: '4~7日で発送' }
 
   ]
 
