07df13e177c0a11a52db92f80910ac47590be96c
zaki09901 <85482210+zaki09901@users.noreply.github.com>
2021-10-14 14:53:17 +0900

    バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,7 +14,7 @@ validates :prefecture_id, numericality: { other_than: 0, message: "can't be blan
 
 validates  :postal_code,presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を入力してください' }
 
-validates :phone_number, presence: true, format: { with: /\A((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}\z/, message: 'を入力してください' }
+validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'を入力してください' }
 
 def save
   order = Order.create(user_id: user_id, item_id: item_id)
